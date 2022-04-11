94dcfa20a4f36da29c965d4cff81d6d5488e30c6
MariZaja <m.zajaczkowska02@gmail.com>
2022-04-11 16:39:37 +0200

    Animal Class init

---
+++ Animal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Marysia on 11.04.2022.
+//
+
+#include "Animal.h"
---
+++ Animal.h
@@ -0,0 +1,14 @@
+//
+// Created by Marysia on 11.04.2022.
+//
+#include "Organism.h"
+#ifndef MYWORLD_ANIMAL_H
+#define MYWORLD_ANIMAL_H
+
+
+class Animal: private Organism {
+
+};
+
+
+#endif //MYWORLD_ANIMAL_H
